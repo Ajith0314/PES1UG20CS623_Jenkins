4640d1d2d1936310549f714331b3002d59bf70f0
Ajith0314 <112875176+Ajith0314@users.noreply.github.com>
2023-02-19 09:11:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Executing Task-4\n";
   cout << "Hello, Jenkins\n";
+  cout << "The process is Automated";
   cout << "I have successfully built and run\n";
   return 0;
 }
